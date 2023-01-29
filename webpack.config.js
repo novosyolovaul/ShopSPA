8a8e6dcda18fe945f8f16daa643480097314ce5c
novosyolovaul <novosyolova.u.13@gmail.com>
2023-01-29 15:00:54 +0300

    publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/ShopSPA',
+        publicPath: '/ShopSPA/',
         assetModuleFilename: 'assets/[hash][ext][query]',
         clean: true,
     },
